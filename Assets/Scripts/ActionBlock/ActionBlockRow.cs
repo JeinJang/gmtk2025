d8f77c7a8fd51004a3cd8f86b9368f8b5ec2b03f
JeinJang <jeinjang99@gmail.com>
2025-08-02 20:31:45 +0900

    feat: #1 - 턴 진행 예시

--- Assets/Scripts/ActionBlock/ActionBlockRow.cs
+++ Assets/Scripts/ActionBlock/ActionBlockRow.cs
@@ -1,6 +1,8 @@
-using UnityEngine;
-using UnityEngine.UI;
+
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
 
 public class ActionBlockRow : MonoBehaviour
 {
@@ -14,6 +16,15 @@ public class ActionBlockRow : MonoBehaviour
     {
         Instance = this;
         container = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        // 디버깅용 : 턴 진행 
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            StartConsumingBlocks();
+        }
     }
 
     public void AddBlock(int columnIndex, ActionBlock? block)
@@ -41,6 +52,39 @@ public class ActionBlockRow : MonoBehaviour
             blocksInRow[columnIndex] = null;
         }
     }
+
+    public void StartConsumingBlocks(float delay = 0.25f)
+    {
+        StartCoroutine(ConsumeBlocksSequentially(delay));
+    }
+
+    public IEnumerator ConsumeBlocksSequentially(float delay)
+    {
+        for (int i = 0; i < blocksInRow.Count; i++)
+        {
+            var block = blocksInRow[i];
+            if (block != null)
+            {
+                // 효과 
+                var tf = block.transform;
+                var rt = tf as RectTransform;
+                if (rt != null)
+                {
+                    rt.pivot = new Vector2(0.5f, 0.5f);
+                }
+                Sequence seq = DOTween.Sequence();
+                seq.Append(tf.DOScale(1.1f, 0.2f).SetEase(Ease.OutBack));
+                seq.Append(tf.DOScale(0.0f, 0.05f).SetEase(Ease.InBack));
+
+                yield return new WaitForSeconds(0.25f);
+            }
+
+            yield return new WaitForSeconds(delay);
+        }
+
+        blocksInRow.Clear(); // 내부 리스트도 정리
+    }
+
     public void ClearBlock()
     {
         foreach (ActionBlock block in blocksInRow)
