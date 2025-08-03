e314bb5576b0cb278e9eb7fb96fa9a8f27bb93da
zerofruit <14wnrkim@gmail.com>
2025-08-03 23:45:52 +0900

    ground tile, spike, flag

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using MoreMountains.Tools;
 using UnityEngine;
 
 namespace DefaultNamespace
@@ -6,15 +9,43 @@ namespace DefaultNamespace
     {
         protected const string _upParameterName = "Up";
         protected int _upAnimationParameter;
+        public virtual HashSet<int> _animatorParameters { get; set; } = new HashSet<int>();
+        public bool IsUp;
         
+        [Header("Test")]
+        [MMInspectorButton("TestUp")]
+        public bool UpButton;
+        [MMInspectorButton("TestDown")]
+        public bool DownButton;
+
+        Animator _animator;
+
+        void Awake()
+        {
+            this._animator = this.GetComponentInChildren<Animator>();
+            MMAnimatorExtensions.AddAnimatorParameterIfExists(_animator, _upParameterName, out _upAnimationParameter, AnimatorControllerParameterType.Bool, _animatorParameters);
+        }
+
+        void TestUp()
+        {
+            this.SpikeUp();
+        }
+
+        void TestDown()
+        {
+            this.SpikeDown();
+        }
+
         public void SpikeUp()
         {
-            
+            IsUp = true;
+            this._animator.SetBool(this._upAnimationParameter, true);
         }
 
         public void SpikeDown()
         {
-            
+            IsUp = false;
+            this._animator.SetBool(this._upAnimationParameter, false);
         }
     }
 }
